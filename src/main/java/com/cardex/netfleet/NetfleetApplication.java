a1e31f0e81d771ac76530c7c0ae89204e3a81a25
Amine Baba <ababa@rbbn.com>
2021-12-11 10:40:54 -0500

    initial commit

---
+++ src/main/java/com/cardex/netfleet/NetfleetApplication.java
@@ -0,0 +1,13 @@
+package com.cardex.netfleet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetfleetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetfleetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cardex/netfleet/NetfleetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardex.netfleet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetfleetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
